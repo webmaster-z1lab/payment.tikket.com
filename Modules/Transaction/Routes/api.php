97972b92ff61ae51b6d41eaeb6ed50e28472810a
Olimar Ferraz <webmaster@z1lab.com.br>
2019-09-10 10:40:42 -0300

    Added micro-optimization to route clojure;

--- Modules/Transaction/Routes/api.php
+++ Modules/Transaction/Routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::middleware('api.v:1,transaction')->prefix('v1')->group(function ()
+Route::middleware('api.v:1,transaction')->prefix('v1')->group(static function ()
 {
     Route::apiResource('transactions', 'TransactionController')->only(['show', 'store', 'destroy']);
 });
